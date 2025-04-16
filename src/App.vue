b1b73a4643993b3bb37f4d5d30ca1f6d6b876242
Maxim <fatyanovmaksim@mail.ru>
2025-04-16 21:59:44 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { LayoutContainer } from '@/components/core'
+</script>
+
+<template>
+    <LayoutContainer>
+        <router-view v-slot="{ Component }">
+            <Transition name="slide">
+                <component :is="Component" />
+            </Transition>
+        </router-view>
+    </LayoutContainer>
+</template>
---
+++ src/components/core/index.ts
@@ -0,0 +1,3 @@
+import LayoutContainer from './layout-container.vue'
+
+export { LayoutContainer }
